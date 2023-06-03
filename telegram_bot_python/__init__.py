96d82e3bb85bbd27dbcd2eb4a12c880d8fc0f1c4
quitegreensky <quitegreensky@gmail.com>
2023-06-03 19:46:00 +0330

    version 1.0.1

--- telegram_bot_python/__init__.py
+++ telegram_bot_python/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __description__ = "Simple way to create normal and web apps using telegram bot api"
 __author__ = "Sina Namadian"
 __email__ = "quitegreensky@gmail.com"
